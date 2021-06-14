6aac27ffd071cfe8d464722236ee683dbdc73a81
kumar889 <kumar.bajura@gmail.com>
2021-06-14 19:01:12 +0545

    Assignment5,commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// create a varible named array
+var array = [1, 2, 3, 'numbers', 'strings'];
+
+// Methods to print arrays
+console.log(array);
+
+console.log(array[0], array[1], array[2], array[3], array[4]);
+
+console.log(array[0] + '\n' + array[1] + '\n' + array[2] + '\n' + array[3] + '\n' + array[4]);
